69df3711ccdf31f55014763011e9090ee9e29ce2
vruizdev <victorherrera1316@gmail.com>
2024-04-09 22:52:00 -0600

    create: structure

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
-<?php
-    phpinfo();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple inventory challenge</title>
+
+    <style>
+        :root {
+            color-scheme: light dark;
+        }
+
+        body {
+            display: grid;
+            place-content: center;
+        }
+</style>
+</head>
+<body>
+    <h1>Hola mundo!</h1>
+
+
+</body>
+</html>
